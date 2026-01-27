61ca4d46d719b67dc9cd0884fa9045e37c8b2d02
saksham <992401030266@mail.jiit.ac.in>
2026-01-28 02:50:02 +0530

    Time: 3 ms (61.71%), Space: 23.5 MB (47.18%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+private:
+    stack<int> st;      // main stack
+    stack<int> minSt;   // stack to track minimum
+
+public:
+    MinStack() {
+        // nothing to initialize
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if (minSt.empty())
+            minSt.push(val);
+        else
+            minSt.push(min(val, minSt.top()));
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
